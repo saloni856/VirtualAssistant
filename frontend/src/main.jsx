f77c7500b6cb81e2d798aff237ae95a8446a2243
saloni856 <vsaloni565@gmail.com>
2025-12-30 06:17:51 +0530

    Update main.jsx

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,14 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from  "react-router-dom"
-import userContext from './context/userContext.jsx'
+import UserContext from './context/UserContext.jsx'
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
 createRoot(document.getElementById('root')).render(
 <BrowserRouter>
-<userContext>
+<UserContext>
     <App />
-</userContext>
+</UserContext>
 </BrowserRouter>
 )
