e246240e721aff505e068f819197068642b3000b
saloni856 <vsaloni565@gmail.com>
2025-12-30 07:12:33 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,4 @@
-// import express from "express"
-// import dotenv from "dotenv"
-// dotenv.config()
-// import connectDb from "./config/db.js"
-// import authRouter from "./routes/auth.routes.js"
-// import cors from "cors"
-// import cookieParser from "cookie-parser"
-// import userRouter from "./routes/user.routes.js"
-// const app = express()
-// app.use(cors({
-//   origin:"http://localhost:5173",
-//   credentials:true
-// }))
-// const port = process.env.PORT || 5000
 
-// app.use(express.json())
-// app.use(cookieParser())
-// app.use("/api/auth",authRouter)
-// app.use("/api/user",userRouter)
-
-// app.listen(port,()=>{
-//   connectDb()
-//   console.log("Server Started")
-// })
 
 import dotenv from "dotenv";
 dotenv.config();
@@ -39,7 +16,7 @@ const app = express();
 
 // FIX 1 — CORS
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://virtualassistant-h91c.onrender.com",
   credentials: true,
 }));
 
