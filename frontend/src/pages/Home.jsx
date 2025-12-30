b9169360b6d6d0ec88896cb2730135b93feb5317
saloni856 <vsaloni565@gmail.com>
2025-12-30 06:54:30 +0530

    Update Home.jsx

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,6 +1,6 @@
 
 import { useContext, useState, useEffect, memo, useCallback, useRef } from "react";
-import { userDataContext } from "../context/userContext";
+import { userDataContext } from "../context/UserContext";
 import { useNavigate } from "react-router-dom";
 import api from "../api";
 import aiImg from "../assets/ai.gif";
